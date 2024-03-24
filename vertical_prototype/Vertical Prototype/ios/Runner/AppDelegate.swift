a8a5b645dd878cd07d67c1d8878eb73f9f4b5969
Bruno Miguel Ataide Fortes <52974677+Bruno081002@users.noreply.github.com>
2024-03-24 00:51:05 +0000

    adding maps

--- vertical_prototype/Vertical Prototype/ios/Runner/AppDelegate.swift
+++ vertical_prototype/Vertical Prototype/ios/Runner/AppDelegate.swift
@@ -1,12 +1,15 @@
 import UIKit
 import Flutter
-
+import GoogleMaps
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices
+        .provideAPIKey
+        ("AIzaSyBs-rqqUizowhqPaA3Iwb0bot-KP0HspK4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
